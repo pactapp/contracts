f2c40a4072a9dd8fa9b16700c4e5100e04f67a96
Gurpreet <gs109690@gmail.com>
2026-01-23 21:54:56 +0530

    code cleanup

---
+++ test/BankTest.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {Bank} from "../src/Bank.sol";
+
+contract CounterTest is Test {
+    Bank public bank;
+}
