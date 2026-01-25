9ebeea3ddcb454b60d159607e73c9f4a9633d01f
Gurpreet <gs109690@gmail.com>
2026-01-25 16:17:09 +0530

    basic deposit logic

--- script/Bank.sol
+++ script/Bank.sol
@@ -6,13 +6,16 @@ import {Bank} from "../src/Bank.sol";
 
 contract BankScript is Script {
     Bank public bank;
+    address public usdc;
 
-    function setUp() public {}
+    function setUp() public {
+        usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; //mainnet usdc
+    }
 
     function run() public {
         vm.startBroadcast();
 
-        bank = new Bank();
+        bank = new Bank(usdc);
 
         vm.stopBroadcast();
     }
--- src/Bank.sol
+++ src/Bank.sol
@@ -1,4 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-contract Bank {}
+import {Owned} from "solmate/auth/Owned.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Bank is Owned {
+    address public usdc;
+
+    uint256 public totalShares;
+
+    uint256 public constant penalty = 10_00; // 10%
+    uint256 public constant protocolFee = 10_00; // 10% on penalty withdraws, total 1% of withdraw amount
+
+    mapping(address => uint256) public shares;
+
+    constructor(address _usdc) Owned(owner) {
+        owner = msg.sender;
+        usdc = _usdc;
+    }
+
+    function deposit(uint256 amount) public {
+        require(amount > 0, "Deposit must be more than 0");
+
+        uint256 assetsBefore = IERC20(usdc).balanceOf(address(this));
+
+        IERC20(usdc).transferFrom(msg.sender, address(this), amount);
+
+        uint256 sharesAlloted;
+
+        if (totalShares == 0) {
+            sharesAlloted = amount;
+        } else {
+            sharesAlloted = amount * totalShares / assetsBefore;
+        }
+
+        totalShares += sharesAlloted;
+        shares[msg.sender] += sharesAlloted;
+    }
+}
